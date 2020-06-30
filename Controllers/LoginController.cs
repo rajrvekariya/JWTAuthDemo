8fdadaeec544328fc54fe994b9e9a29a43ee1b6a
raj vekariya <raj.r.vekariya@gmail.com>
2020-06-30 09:11:11 +0530

    Add project files.

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using JWTAuthDemo.Model;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+namespace JWTAuthDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        public IConfiguration _configuration;
+
+        public LoginController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public IActionResult Login(string username, string pass)
+        {
+            UserModel login = new UserModel();
+            login.UserName = username;
+            login.Password = pass;
+            IActionResult response = Unauthorized();
+
+            var user = AuthenticateUser(login);
+            if (user != null)
+            {
+                var tokenStr = GenerateJsonWebToken(user);
+                response = Ok(new { token = tokenStr });
+            }
+
+            return response;
+        }
+
+        private UserModel AuthenticateUser(UserModel login)
+        {
+            UserModel user = null;
+            if (login.UserName == "username123" && login.Password == "pass123")
+            {
+                user = new UserModel { UserName = "username123", EmailAddress = "Email@Eamil.com", Password = "pass123" };
+            }
+            return user;
+        }
+
+        string GenerateJsonWebToken(UserModel userInfo)
+        {
+            var securithToken = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWT:Key"]));
+            var credential = new SigningCredentials(securithToken, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[]
+            {
+                new Claim(System.IdentityModel.Tokens.Jwt.JwtRegisteredClaimNames.Sub,userInfo.UserName),
+                new Claim(System.IdentityModel.Tokens.Jwt.JwtRegisteredClaimNames.Email,userInfo.EmailAddress),
+                new Claim(System.IdentityModel.Tokens.Jwt.JwtRegisteredClaimNames.Jti,Guid.NewGuid().ToString())
+            };
+
+            var token = new JwtSecurityToken(
+                issuer: _configuration["jwt:Issuer"],
+                audience: _configuration["jwt:Issuer"],
+                claims,
+                expires: DateTime.Now.AddMinutes(120),
+                signingCredentials: credential);
+
+            var encodedToken = new JwtSecurityTokenHandler().WriteToken(token);
+            return encodedToken;
+        }
+
+        [Authorize]
+        [HttpPost("Welcome")]
+        public string Welcome()
+        {
+            var identity = HttpContext.User.Identity as ClaimsIdentity;
+            IList<Claim> claims = identity.Claims.ToList();
+            var userName = claims[0].Value;
+            return "Welc: " + userName;
+        }
+    }
+}
